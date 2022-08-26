9a59001a1c70ecd672025317613a4c1cb350e0c5
Lars Haugan <lars@larshaugan.net>
2022-08-26 08:44:08 +0200

    upgrade k8s libs and go version

--- pkg/k8s/tokenreview.go
+++ pkg/k8s/tokenreview.go
@@ -1,21 +1,23 @@
 package k8s
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 
 	authenticationapi "k8s.io/api/authentication/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 //WhoAmI returns the current user/token subject
 func WhoAmI(kubeclient kubernetes.Interface, token string) (string, *authenticationapi.TokenReviewStatus, error) {
-	result, err := kubeclient.AuthenticationV1().TokenReviews().Create(&authenticationapi.TokenReview{
+	result, err := kubeclient.AuthenticationV1().TokenReviews().Create(context.Background(), &authenticationapi.TokenReview{
 		Spec: authenticationapi.TokenReviewSpec{
 			Token: token,
 		},
-	})
+	}, v1.CreateOptions{})
 	if err != nil {
 		if k8serrors.IsForbidden(err) {
 			return getUsernameFromError(err), nil, nil
